e1a6d425aea57e5937851223c74d5dd8309cb032
chriss <krzysztof.andrzejczak@findable.pl>
2018-05-15 18:44:51 +0200

    Why this does not work

--- src/main/java/pl/findable/uptime/UptimeMonitor.java
+++ src/main/java/pl/findable/uptime/UptimeMonitor.java
@@ -2,13 +2,20 @@ package pl.findable.uptime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
-public class UptimeMonitor {
+public class UptimeMonitor extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UptimeMonitor.class, args);
