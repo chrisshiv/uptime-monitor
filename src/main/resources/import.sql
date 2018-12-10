f79bc5444d17bd638361e453cb72ceed43a7615d
chriss <krzysztof.andrzejczak@findable.pl>
2018-12-10 17:20:34 +0100

    Fast -> FG

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,7 @@
 insert into site (name, url, interval) values ('Abriso', 'http://www.abriso.com.pl', 1);
 insert into site (name, url, interval) values ('Anba21', 'https://www.anba21.pl', 1);
-insert into site (name, url, interval) values ('Fast', 'http://www.fast.pl', 1);
+-- insert into site (name, url, interval) values ('Fast', 'http://www.fast.pl', 1);
+insert into site (name, url, interval) values ('FG', 'http://fotogalerie.pl', 1);
 insert into site (name, url, interval) values ('ISEO', 'http://iseo.pl', 1);
 insert into site (name, url, interval) values ('Koblenz', 'http://www.koblenz.pl', 1);
 insert into site (name, url, interval) values ('Lineacali', 'http://lineacali.findable.pl', 1);
